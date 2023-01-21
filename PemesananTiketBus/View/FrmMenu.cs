64a91099454bb66d250fab0c35f3d35130fb281f
Anggita Dika Tamtama <97569405+Q1B0@users.noreply.github.com>
2023-01-21 13:18:21 +0700

    FormMenu.cs udh dikomen semua

--- PemesananTiketBus/View/FrmMenu.cs
+++ PemesananTiketBus/View/FrmMenu.cs
@@ -12,32 +12,50 @@ namespace PemesananTiketBus.View
 {
     public partial class FrmMenu : Form
     {
+        // Membuat method constructor yang digunakan untuk menginisialisasi komponen-komponen
         public FrmMenu()
         {
+            // Membuat method untuk menginisialisasi seluruh komponen
             InitializeComponent();
         }
 
+        // Method ini akan dijalankan ketika tombol dengan ID btnBus diklik.
         private void btnBus_Click(object sender, EventArgs e)
         {
+            // Membuat objek baru dari form FrmDataBus
             FrmDataBus frm = new FrmDataBus();
+
+            // Menampilkan form FrmDataBus dengan menggunakan ShowDialog()
             frm.ShowDialog();
         }
 
+        // Method ini akan dijalankan ketika tombol dengan ID btnPelanggan diklik.
         private void btnPelanggan_Click(object sender, EventArgs e)
         {
+            // Membuat objek baru dari form FrmDataPelanggan
             FrmDataPelanggan frm = new FrmDataPelanggan();
+
+            // Menampilkan form FrmDataPelanggan dengan menggunakan ShowDialog()
             frm.ShowDialog();
         }
 
+        // Method ini akan dijalankan ketika tombol dengan ID btnTiket diklik.
         private void btnTiket_Click(object sender, EventArgs e)
         {
+            // Membuat objek baru dari form FrmTiket
             FrmTiket frm = new FrmTiket();
+
+            // Menampilkan form FrmTiket dengan menggunakan ShowDialog()
             frm.ShowDialog();
         }
 
+        // Method ini akan dijalankan ketika tombol dengan ID btnPembayaran diklik.
         private void btnPembayaran_Click(object sender, EventArgs e)
         {
+            // Membuat objek baru dari form FrmPembayaran
             FrmPembayaran frm = new FrmPembayaran();
+
+            // Menampilkan form FrmPembayaran dengan menggunakan ShowDialog()
             frm.ShowDialog();
         }
     }
