ab38de850b15938dba8461dbc1e7f4afff0d5e3a
Anggita Dika Tamtama <97569405+Q1B0@users.noreply.github.com>
2023-01-21 13:06:35 +0700

    FrmPembayaran.cs udh selesai dikomen

--- PemesananTiketBus/View/FrmPembayaran.cs
+++ PemesananTiketBus/View/FrmPembayaran.cs
@@ -16,67 +16,115 @@ namespace PemesananTiketBus.View
 {
     public partial class FrmPembayaran : Form
     {
+        // Deklarasi objek controllerPembayran dari kelas PemabayranController
         private PembayaranController controllerPembayaran = new PembayaranController();
+
+        // Deklarasi objek controllerTiket dari kelas TiketController
         private TiketController controllerTiket = new TiketController();
+
+        // Deklarasi objek pembayaran dari kelas Pembayaran
         private Pembayaran pembayaran = new Pembayaran();
+
+        // Deklarasi objek listOfPembayaran sebagai list dari kelas Pembayaran
         private List<Pembayaran> listOfPembayaran = new List<Pembayaran>();
+
+        // Method yang akan dipanggil saat form dibuka
         public FrmPembayaran()
         {
+            // Memanggil method InitializeComponent untuk menginisialisasi komponen yang digunakan dalam form
             InitializeComponent();
+
+            // Mematikan fitur teks di textNamaPelanggan
             textNamaPelanggan.Enabled = false;
+
+            // Mematikan fitur teks di textTotalBayar
             textTotalBayar.Enabled = false;
+
+            // Memanggil method InitListView untuk mengatur tampilan listview
             InitListView();
+
+            // Memanggil method LoadData untuk mengambil data dari database
             LoadData();
+
+            // Memanggil method Reset untuk mengembalikan semua input ke kondisi awal
             Reset();
         }
 
-        
+        // Membuat event ketika button cari ID tiket diklik
         private void btnCariIDTiket_Click(object sender, EventArgs e)
         {
+            // Mencoba untuk mengambil data tiket berdasarkan ID yang diinputkan
             try
             {
                 pembayaran.Tiket = controllerTiket.ReadById(Convert.ToInt32(textIDTiket.Text));
             }
+
+            // Jika terjadi kesalahan, maka akan masuk ke catch
             catch
             {
 
             }
+
+            // Jika data tiket yang diinputkan tidak ditemukan
             if(pembayaran.Tiket == null)
             {
+                // Muncul pesan peringatan bahwa tiket tidak ditemukan
                 MessageBox.Show("Tiket tidak ditemukan !!!", "Peringatan",
-                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-            } else
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            } 
+
+            // Jika data tiket ditemukan
+            else
             {
+                // Maka akan menampilkan nama pelanggan dan total bayar di textbox yang sesuai
                 textNamaPelanggan.Text = pembayaran.Tiket.Pelanggan.Nama;
                 textTotalBayar.Text = pembayaran.TotalBayar.ToString();
             }
         }
 
-        // load data dan tampilkan ke listview
+        // Load data dan tampilkan ke listview
         private void LoadData()
         {
+            // Menghapus semua item di list view pembayaran
             listPembayaran.Items.Clear();
+
+            // Mendapatkan list dari data pembayaran yang ada di database
             listOfPembayaran = controllerPembayaran.ReadAll();
+
+            // Looping setiap data pembayaran yang ada di list
             foreach (var item in listOfPembayaran)
             {
+                // Mendapatkan nomor urut dari data pembayaran
                 var no = listPembayaran.Items.Count + 1;
+
+                // Membuat objek baru dari list view item
                 var listItem = new ListViewItem(no.ToString());
+
+                // Menambahkan sub item untuk setiap data pembayaran
                 listItem.SubItems.Add(item.IDPembayaran);
                 listItem.SubItems.Add(item.Tiket.Pelanggan.Nama);
                 listItem.SubItems.Add(item.Tiket.IDTiket);
                 listItem.SubItems.Add(item.TotalBayar.ToString());
                 listItem.SubItems.Add(item.TanggalBayar.ToString("dd/MM/yy"));
 
+                // Menambahkan objek list view item ke list view pembayaran
                 listPembayaran.Items.Add(listItem);
             }
         }
 
+        // Method untuk inisialisasi list view untuk menampilkan data pembayaran
         private void InitListView()
         {
+            // Mengatur tampilan list view menjadi detail
             listPembayaran.View = System.Windows.Forms.View.Details;
+
+            // Mengaktifkan pilihan untuk menampilkan seluruh baris ketika dipilih
             listPembayaran.FullRowSelect = true;
+
+            //Mengaktifkan garis grid di list view
             listPembayaran.GridLines = true;
 
+            // Mengaktifkan garis grid di list view
             listPembayaran.Columns.Add("No.", 30, HorizontalAlignment.Center);
             listPembayaran.Columns.Add("ID Bayar", 100, HorizontalAlignment.Center);
             listPembayaran.Columns.Add("Nama Pelanggan", 130, HorizontalAlignment.Center);
@@ -85,6 +133,7 @@ namespace PemesananTiketBus.View
             listPembayaran.Columns.Add("Tanggal Bayar", 150, HorizontalAlignment.Center);
         }
 
+        // Membuat event handler cuntuk buttob Bayar
         private void btnBayar_Click(object sender, EventArgs e)
         {
             pembayaran.IDPembayaran = textIDBayar.Text;
@@ -95,23 +144,33 @@ namespace PemesananTiketBus.View
             Reset();
         }
 
-        // button untuk pergi ke form laporan
+        // Deklarasi method yang akan dijalankan ketika tombol "btnLaporan" diklik.
         private void btnLaporan_Click(object sender, EventArgs e)
         {
+            // Deklarasi dan instansiasi objek baru dari kelas "FrmLaporan"
             FrmLaporan frm = new FrmLaporan();
+
+            // Perintah menampilkan objek "frm" sebagai form dialog
             frm.ShowDialog();
         }
+
+        // Deklarasi method Reset
         private void Reset()
         {
+            // Mengosongkan isi textbox dan mengatur ulang tanggal pada dateBayar
             textIDBayar.Clear();
             dateBayar.ResetText();
             textIDTiket.Clear();
             textNamaPelanggan.Clear();
             textTotalBayar.Clear();
 
+            // Memberikan fokus pada textbox dengan ID textIDBayar
             textIDBayar.Focus();
+
+            // Menonaktifkan tombol dengan ID btnDelete
             btnDelete.Enabled = false;
         }
+
         private void btnDelete_Click(object sender, EventArgs e)
         {
             if (listPembayaran.SelectedItems.Count > 0)
@@ -133,23 +192,31 @@ namespace PemesananTiketBus.View
                 }
             }
 
-            else // data belum dipilih
+            else // Data belum dipilih
             {
                 MessageBox.Show("Data Bus belum dipilih !!!", "Peringatan",
-                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
             }
         }
 
+        // Ketika ada item yang dipilih dari listbox dengan ID listPembayaran, maka akan dilakukan proses pengambilan
+        // data dari objek pembayaran yang dipilih dan menampilkannya pada form.
         private void listPembayaran_SelectedIndexChanged(object sender, EventArgs e)
         {
+            // Jika listbox tidak memiliki item yang dipilih, maka tidak akan dilakukan apa-apa.
             if (listPembayaran.SelectedIndices.Count > 0)
             {
+                // Mengambil objek pembayaran yang dipilih dari listOfPembayaran berdasarkan indeks yang dipilih dari listbox
                 pembayaran = listOfPembayaran[listPembayaran.SelectedIndices[0]];
+
+                // Menampilkan seluruh item pada textbox
                 textIDBayar.Text = pembayaran.IDPembayaran;
                 dateBayar.Value = pembayaran.TanggalBayar;
                 textIDTiket.Text = pembayaran.Tiket.IDTiket;
                 textNamaPelanggan.Text = pembayaran.Tiket.Pelanggan.Nama;
                 textTotalBayar.Text = pembayaran.TotalBayar.ToString();
+
+                // Mengaktifkan tombol dengan ID btnDelete
                 btnDelete.Enabled = true;
             }
         }
