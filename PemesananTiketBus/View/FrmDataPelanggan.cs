4c5155bd5037dc8124b324b5705bd3ee3c26894a
Anggita Dika Tamtama <97569405+Q1B0@users.noreply.github.com>
2023-01-21 19:45:01 +0700

    FrmDataPelanggan.cs udah dikomen semua

--- PemesananTiketBus/View/FrmDataPelanggan.cs
+++ PemesananTiketBus/View/FrmDataPelanggan.cs
@@ -15,27 +15,44 @@ namespace PemesananTiketBus.View
 {
     public partial class FrmDataPelanggan : Form
     {
-        // deklarasi objek controller
+        // Deklarasi objek controller dari kelas PelangganController
         private PelangganController controller = new PelangganController();
 
+        // Deklarasi objek listOfPelanggan dari tipe List<Pelanggan>
         private List<Pelanggan> listOfPelanggan = new List<Pelanggan>();
 
+        // Deklarasi objek pelanggan dari tipe Pelanggan
         private Pelanggan pelanggan = new Pelanggan();
 
+        // Konstruktor dari FrmDataPelanggan
         public FrmDataPelanggan()
         {
+            // Memanggi method untuk inisialisasi setiap komponen
             InitializeComponent();
+
+            // Memanggi method untuk inisialisasi tampilan ListView
             InisialisasiListView();
+
+            // Memanggil method untuk memuat data pelanggan dari database
             LoadDataPelanggan();
+
+            // Memanggil method untuk mereset form ke kondisi awal
             ResetForm();
         }
 
+        // Membuat method yang digunakan untuk inisialisasi tampilan ListView
         private void InisialisasiListView()
         {
+            // Menetapkan tampilan ListView sebagai detail
             listDataPelanggan.View = System.Windows.Forms.View.Details;
+
+            // Menetapkan ListView agar memilih seluruh baris saat satu baris dipilih
             listDataPelanggan.FullRowSelect = true;
+
+            //Menampilkan garis grid pada ListView
             listDataPelanggan.GridLines = true;
 
+            // Menambahkan kolom-kolom pada list view dan menentukan lebar dan posisi tiap kolom
             listDataPelanggan.Columns.Add("No.", 30, HorizontalAlignment.Center);
             listDataPelanggan.Columns.Add("ID", 60, HorizontalAlignment.Center);
             listDataPelanggan.Columns.Add("Nama", 150, HorizontalAlignment.Center);
@@ -44,118 +61,148 @@ namespace PemesananTiketBus.View
             listDataPelanggan.Columns.Add("Email", 135, HorizontalAlignment.Center);
         }
 
-        // method untuk menampilkan semua data mahasiswa
+        // Membuat method yang digunakan untuk memuat data pelanggan dari database
         private void LoadDataPelanggan()
         {
-            // kosongkan listview
+            // Menghapus semua item pada ListView
             listOfPelanggan = controller.ReadAll();
+
+            // Membersihkan list view sebelum menampilkan data baru
             listDataPelanggan.Items.Clear();
 
-            // ekstrak objek mhs dari collection
+            // Perulangan untuk mengambil setiap objek pelanggan dari listOfPelanggan
             foreach (var pelanggan in listOfPelanggan)
             {
+                // Menghitung urutan nomor dari setiap item pada ListView
                 var noUrut = listDataPelanggan.Items.Count + 1;
 
+                // Membuat objek item dari tipe ListViewItem
                 var item = new ListViewItem(noUrut.ToString());
+
+                // Menambahkan subitem pada objek item dengan data dari objek pelanggan
                 item.SubItems.Add(pelanggan.IdPelanggan);
                 item.SubItems.Add(pelanggan.Nama);
                 item.SubItems.Add(pelanggan.No_tlp);
                 item.SubItems.Add(pelanggan.Alamat);
                 item.SubItems.Add(pelanggan.Email);
 
-                // tampilkan data mhs ke listview
+                // Menambahkan objek item ke ListView
                 listDataPelanggan.Items.Add(item);
             }
         }
 
+        // Method yang akan dijalankan saat tombol Simpan diklik
         private void btnSimpan_Click(object sender, EventArgs e)
         {
-            // set nilai property objek mahasiswa yg diambil dari TextBox
+            // Mengisi properti dari objek pelanggan dengan data dari form
             pelanggan.IdPelanggan = textIDPelanggan.Text;
             pelanggan.Nama = textNama.Text;
             pelanggan.No_tlp = textNoTelp.Text;
             pelanggan.Alamat = textAlamat.Text;
             pelanggan.Email = textEmail.Text;
 
+            // Deklarasi variabel result untuk menyimpan hasil dari method Create()
             int result = 0;
 
-            // panggil operasi CRUD
+            // Menjalankan method Create() dari objek controller dengan parameter objek pelanggan
             result = controller.Create(pelanggan);
 
-            if (result > 0) // tambah data berhasil
+            // Jika hasil dari method Create() lebih dari 0 (berhasil)
+            if (result > 0)
             {
-                LoadDataPelanggan();// panggil event OnCreate
+                // Memuat ulang data pelanggan pada ListView
+                LoadDataPelanggan();
 
-                // reset form input, utk persiapan input data berikutnya
-
+                // Menjalankan tombol Reset secara otomatis
                 btnReset.PerformClick();
             }
         }
 
+        // Method yang akan dijalankan saat tombol Update diklik
         private void btnUpdate_Click(object sender, EventArgs e)
         {
+            // Jika ada item yang dipilih pada ListView
             if (listDataPelanggan.SelectedItems.Count > 0)
             {
-                // set nilai property objek mahasiswa yg diambil dari TextBox
+                // Mengisi properti dari objek pelanggan dengan data dari form
                 pelanggan.IdPelanggan = textIDPelanggan.Text;
                 pelanggan.Nama = textNama.Text;
                 pelanggan.No_tlp = textNoTelp.Text;
                 pelanggan.Alamat = textAlamat.Text;
                 pelanggan.Email = textEmail.Text;
 
+                // Deklarasi variabel result untuk menyimpan hasil dari method Update()
                 int result = 0;
 
-                // panggil operasi CRUD
+                // Menjalankan method Update() dari objek controller dengan parameter objek pelanggan
                 result = controller.Update(pelanggan);
 
-                if (result > 0) // tambah data berhasil
+                // Jika hasil dari method Update() lebih dari 0 (berhasil)
+                if (result > 0) 
                 {
-                    LoadDataPelanggan();// panggil event OnCreate
+                    // Memuat ulang data pelanggan pada ListView
+                    LoadDataPelanggan();
 
-                    // reset form input, utk persiapan input data berikutnya
-
+                    // Menjalankan tombol Reset secara otomatis
                     btnReset.PerformClick();
                 }
             }
-            else // data belum dipilih
+
+            // Jika tidak ada item yang dipilih pada ListView
+            else
             {
+                // Menampilkan pesan peringatan
                 MessageBox.Show("Data belum dipilih", "Peringatan", MessageBoxButtons.OK,
-                        MessageBoxIcon.Exclamation);
+                MessageBoxIcon.Exclamation);
             }
         }
 
+        // Method yang akan dijalankan saat tombol Delete diklik
         private void btnDelete_Click(object sender, EventArgs e)
         {
+            // Jika ada item yang dipilih pada ListView
             if (listDataPelanggan.SelectedItems.Count > 0)
             {
+                // Menampilkan konfirmasi untuk menghapus data
                 var konfirmasi = MessageBox.Show("Yakin ingin Hapus Data Pelanggan?", "Konfirmasi",
-                        MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+                MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
 
+                //Jika user menekan tombol Yes
                 if (konfirmasi == DialogResult.Yes)
                 {
-                    // ambil objek mhs yang mau dihapus dari collection
+                    // Mendapatkan objek pelanggan yang dipilih dari listOfPelanggan
                     Pelanggan pelanggan = listOfPelanggan[listDataPelanggan.SelectedIndices[0]];
 
-                    // panggil operasi CRUD
+                    // Menjalankan method Delete() dari objek controller dengan parameter objek pelanggan
                     var result = controller.Delete(pelanggan);
+
+                    // Jika hasil dari method Delete() lebih dari 0 (berhasil)
                     if (result > 0) LoadDataPelanggan();
 
+                    // Menjalankan tombol Reset secara otomatis
                     btnReset.PerformClick();
                 }
             }
 
-            else // data belum dipilih
+            // Jika tidak ada item yang dipilih pada ListView
+            else
             {
+                // Menampilkan pesan peringatan
                 MessageBox.Show("Data Pelanggan belum dipilih !!!", "Peringatan",
-                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
             }
         }
 
+        // Method yang akan dijalankan saat ada perubahan pilihan pada ListView
         private void listDataPelanggan_SelectedIndexChanged(object sender, EventArgs e)
         {
+            // Jika ada item yang dipilih pada ListView
             if (listDataPelanggan.SelectedIndices.Count > 0)
             {
+                // Mendapatkan objek pelanggan yang dipilih dari listOfPelanggan
                 pelanggan = listOfPelanggan[listDataPelanggan.SelectedIndices[0]];
+
+                // Menampilkan data dari objek pelanggan ke form
                 textIDPelanggan.Text = pelanggan.IdPelanggan;
                 textIDPelanggan.Enabled = false;
                 textNama.Text = pelanggan.Nama;
@@ -163,13 +210,16 @@ namespace PemesananTiketBus.View
                 textAlamat.Text = pelanggan.Alamat;
                 textEmail.Text = pelanggan.Email;
 
-                btnDelete.Enabled = true; // enable button Hapus saat data dalam listview dipilih
-                btnUpdate.Enabled = true; // enable button Update saat data dalam listview dipilih
+                // Mengaktifkan tombol Delete dan Update
+                btnDelete.Enabled = true;
+                btnUpdate.Enabled = true; 
             }
         }
 
+        // Membuat method untuk mengembalikan Form ke kondisi awal
         private void ResetForm()
         {
+            // Membersihkan inputan pada form
             textIDPelanggan.Clear();
             textIDPelanggan.Enabled = true;
             textNama.Clear();
@@ -177,13 +227,18 @@ namespace PemesananTiketBus.View
             textAlamat.Clear();
             textEmail.Clear();
 
+            //Menetapkan fokus pada textIDPelanggan
             textIDPelanggan.Focus();
+
+            // Menonaktifkan tombol Delete dan Update
             btnDelete.Enabled = false;
             btnUpdate.Enabled = false;
         }
 
+        // Method yang akan dijalankan saat tombol Reset diklik
         private void btnReset_Click(object sender, EventArgs e)
         {
+            // Memanggil method ResetForm()
             ResetForm();
         }
     }
