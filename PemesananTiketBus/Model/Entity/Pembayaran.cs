05419c282638789e371f9f690e8e90ca52aecd26
Anggita Dika Tamtama <97569405+Q1B0@users.noreply.github.com>
2023-01-23 20:07:15 +0700

    Update Pembayaran.cs
    komen

--- PemesananTiketBus/Model/Entity/Pembayaran.cs
+++ PemesananTiketBus/Model/Entity/Pembayaran.cs
@@ -6,12 +6,15 @@ using System.Threading.Tasks;
 
 namespace PemesananTiketBus.Model.Entity
 {
+    // Deklarasi kelas Pembayaran
     public class Pembayaran
     {
+        // Membuat properti yang dapat diakses dari luar kelas
         public string IDPembayaran { get; set; }
         public DateTime TanggalBayar { get; set; }
         public int TotalBayar {
             get {
+                // kode yang digunakan untuk menghitung total harga yang harus dibayar
                 return Tiket.Bus.Harga * Tiket.JumlahTiket;
             }
         }
